f45016ca1fb79270d9b613b8264238e93d33546e
Lmntria <seymur1095@gmail.com>
2022-11-02 14:43:33 +0400

    task with flatmap

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,24 +22,32 @@ let forthArr=[15,16,17,18,19]
 let fifthArr=[51,52,53,54,55,56]
 
 
-function sortArrays(firstArr,secondArr,third,fourth,fifth) {
-  let newArr=[]
-  if (third==null && fourth==null && fifth==null) {
-    newArr=[].concat(firstArr,secondArr).sort((x,y)=>x-y)
-    return newArr
-  }
-  else if (third!=null && fourth==null && fifth==null) {
-    newArr=[].concat(firstArr,secondArr,thirdArr).sort((x,y)=>x-y)
-    return newArr
-  }
-  else if (third!=null && fourth!=null && fifth==null) {
-    newArr=[].concat(firstArr,secondArr,thirdArr,fourth).sort((x,y)=>x-y)
-    return newArr
-  }
-  else if (third!=null && fourth!=null && fifth!=null) {
-    newArr=[].concat(firstArr,secondArr,thirdArr,fourth,fifth).sort((x,y)=>x-y)
-    return newArr
-  }
+// function sortArrays(firstArr,secondArr,third,fourth,fifth) {
+//   let newArr=[]
+//   if (third==null && fourth==null && fifth==null) {
+//     newArr=[].concat(firstArr,secondArr).sort((x,y)=>x-y)
+//     return newArr
+//   }
+//   else if (third!=null && fourth==null && fifth==null) {
+//     newArr=[].concat(firstArr,secondArr,thirdArr).sort((x,y)=>x-y)
+//     return newArr
+//   }
+//   else if (third!=null && fourth!=null && fifth==null) {
+//     newArr=[].concat(firstArr,secondArr,thirdArr,fourth).sort((x,y)=>x-y)
+//     return newArr
+//   }
+//   else if (third!=null && fourth!=null && fifth!=null) {
+//     newArr=[].concat(firstArr,secondArr,thirdArr,fourth,fifth).sort((x,y)=>x-y)
+//     return newArr
+//   }
+// }
+
+// console.log(sortArrays(firstArr,secondArr,thirdArr,forthArr,fifthArr))
+
+function combineArrays(firstArr,...restOfArr) {
+  let newArr=restOfArr.flatMap((num)=>num)
+  let combineArr=newArr.concat(firstArr).sort((x,y)=>x-y)
+  return combineArr
 }
 
-console.log(sortArrays(firstArr,secondArr,thirdArr,forthArr,fifthArr))
+console.log(combineArrays(firstArr,secondArr,thirdArr,forthArr,fifthArr))
